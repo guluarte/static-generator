42fb9fd2c50048979d8aac60022273e745907dfb
Rodolfo Guluarte <rodolfo@qrubit.com>
2013-09-21 22:52:18 -0700

    Added Twitter meta tags

--- themes/funny/header.php
+++ themes/funny/header.php
@@ -15,21 +15,20 @@
       <link href="/assets/themes/twitter/bootstrap/css/bootstrap.2.2.2.min.css?version=<?php echo $site['version'];?>" rel="stylesheet" type="text/css" >
       <link href="/assets/themes/twitter/css/style.css?body=1&version=<?php echo $site['version'];?>" rel="stylesheet" type="text/css" media="all">
       <link rel="shortcut icon" href="/favicon.ico?version=<?php echo $site['version'];?>">
-     
+
       <link rel="apple-touch-icon" href="/assets/images/apple-touch-icon.png?version=<?php echo $site['version'];?>">
       <link rel="apple-touch-icon" sizes="72x72" href="/assets/images/apple-touch-icon-72x72.png?version=<?php echo $site['version'];?>">
       <link rel="apple-touch-icon" sizes="114x114" href="/assets/images/apple-touch-icon-114x114.png?version=<?php echo $site['version'];?>">
-     
-     <meta name="description" content="<?php echo $post['title']; ?>">
+
+      <meta name="description" content="<?php echo $post['title']; ?>">
       <!-- Facebook metatags -->
       <?php 
       if(isset($post['image'])):
-          $image = $site['image_path']."thumb480x360.".$post['image'].".jpg";
-        else: 
-          $image = $site['url']."/assets/images/default.jpg?version=".$site['version'];
-        endif; 
+        $image = $site['image_path']."thumb480x360.".$post['image'].".jpg";
+      else: 
+        $image = $site['url']."/assets/images/default.jpg?version=".$site['version'];
+      endif; 
       ?>
-
       <meta property="og:image" content="<?php echo $image; ?>"/>
       <meta property="og:title" content="<?php echo $post['title']; ?>"/>
       <meta property="og:description" content="<?php echo $post['title']; ?>" />
@@ -39,36 +38,46 @@
       <meta property="fb:admins" content="547998939, 100004691232252"/>
       <meta property="fb:page_id" content="165172277018867" />
       <meta property="fb:app_id" content="583195078383535" />
-
-
       <!-- end facebook metatags -->
-      <link href="/assets/custom.css?version=<?php echo $site['version'];?>" rel="stylesheet" type="text/css" >
+      <?php if(isset($post['image'])): ?>
+      <!-- Twitter meta tags -->
+      <meta name="twitter:card" content="photo">
+      <meta name="twitter:url" content="<?php echo $site['url']; ?><?php echo $post['url']; ?>">
+      <meta name="twitter:title" content="<?php echo $post['title']; ?>">
+      <meta name="twitter:description" content="<?php echo $post['title']; ?> @funnythings247">
+      <meta name="twitter:image:src" content="<?php echo $site['image_path'].$post['image']; ?>">
+      <meta name="twitter:site" content="@funnythings247">
+      <meta name="twitter:creator" content="@funnythings247">
+      <meta name="twitter:domain" content="funnythings247.com">
+      <!-- end twitter tags -->
+    <?php endif; ?>
+    <link href="/assets/custom.css?version=<?php echo $site['version'];?>" rel="stylesheet" type="text/css" >
 
-    </head>
+  </head>
 
-    <body>
-      <div id="fb-root"></div>
-      <script>(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=583195078383535";
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
+  <body>
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=583195078383535";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
 
-      <header class="container-fluid width940">
-        <div class="row-fluid menu">
-          <div class="span4">
-            <a href="/" class="logo"><h1>Funny Things 24/7</h1></a>
-          </div>
-          <div class="span7">
-            <div class="hidden-phone">
-             <script type="text/javascript"><!--
-             google_ad_client = "ca-pub-0274242170859142";
-             /* funnthings728x90 */
-             google_ad_slot = "3611419966";
-             google_ad_width = 728;
-             google_ad_height = 90;
+    <header class="container-fluid width940">
+      <div class="row-fluid menu">
+        <div class="span4">
+          <a href="/" class="logo"><h1>Funny Things 24/7</h1></a>
+        </div>
+        <div class="span7">
+          <div class="hidden-phone">
+           <script type="text/javascript"><!--
+           google_ad_client = "ca-pub-0274242170859142";
+           /* funnthings728x90 */
+           google_ad_slot = "3611419966";
+           google_ad_width = 728;
+           google_ad_height = 90;
 //-->
 </script>
 <script type="text/javascript"
